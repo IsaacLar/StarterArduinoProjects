7226802fe0f60549a40a878b3ff276f115899ebc
IsaacLar <isaaclarssen@gmail.com>
2026-09-01 22:05:25 +0100

    Completed Chill-o-meter using temperature sensor

--- Chill-o-meter/src/main.cpp
+++ Chill-o-meter/src/main.cpp
@@ -1,4 +1,49 @@
 #include <Arduino.h>
 
 const int sensorPin = A0;
-const float baselineTemp = 20.0;
+const float baselineTemp = 33.0;
+
+void setup(){
+    Serial.begin(9600);
+
+    //Set LED pins as OUTPUTs and turn them off
+    for(int pinNumber = 2; pinNumber<5; pinNumber++){
+        pinMode(pinNumber, OUTPUT);
+        digitalWrite(pinNumber, LOW);
+    }
+}
+
+void loop(){
+    int sensorVal = analogRead(sensorPin);
+
+    Serial.print("Sensor Value: ");
+    Serial.print(sensorVal);
+
+    float voltage = (sensorVal/1024.0) * 5.0;
+
+    Serial.print(", Volts: ");
+    Serial.print(voltage);
+
+    float temp = (voltage - .5) * 100;
+    Serial.print(", degrees C: ");
+    Serial.println(temp);
+
+    if (temp < baselineTemp + 2){
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+    }else if (temp >= baselineTemp + 2 && temp < baselineTemp + 4){
+        digitalWrite(2, 1);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+    }else if (temp >= baselineTemp + 4 && temp < baselineTemp + 6){
+        digitalWrite(2, 1);
+        digitalWrite(3, 1);
+        digitalWrite(4, 0);
+    }else if (temp >= baselineTemp + 6){
+        digitalWrite(2, 1);
+        digitalWrite(3, 1);
+        digitalWrite(4, 1);
+    }
+    delay(1);
+}
